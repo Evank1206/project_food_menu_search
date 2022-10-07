473273fc58bd31afed54c502f3decc5640f0759c
Evank1206 <jamessain_66@yahoo.com>
2022-10-06 23:46:20 -0700

    search loader icon on display

--- src/js/base_DOM.js
+++ src/js/base_DOM.js
@@ -2,4 +2,6 @@ export const domElement = {
     input__field : document.querySelector(".search__field"),
     submit_search : document.querySelector(".search"),
     search_result : document.querySelector(".results__list"),
+    main : document.querySelector(".recipe"),
+
 }
--- src/js/view/viewSearch.js
+++ src/js/view/viewSearch.js
@@ -6,19 +6,30 @@ const render_Rec = (e) => {
     // console.log(e.title);
     // console.log(e.image_url);
 
-    let list = `<li>
-    <a class="results__link results__link--active" href=${e.recipe_id}>
-        <figure class="results__fig">
-            <img src=${e.image_url} alt="Test">
-        </figure>
-        <div class="results__data">
-            <h4 class="results__name">${e.title}</h4>
-            <p class="results__author">${e.publisher}</p>
-        </div>
-    </a>
-</li>`;
+    let list = 
+    `<li>
+        <a class="results__link results__link--active" href=${e.recipe_id}>
+            <figure class="results__fig">
+                <img src=${e.image_url} alt="Test">
+            </figure>
+            <div class="results__data">
+                <h4 class="results__name">${e.title}</h4>
+                <p class="results__author">${e.publisher}</p>
+            </div>
+        </a>
+    </li>`;
+
+let loading = 
+    `<div class="loader">
+        <svg class="likes__icon">
+            <use href="img/icons.svg#icon-cw"></use>
+        </svg>
+    </div>`;
 // to display to the DOM 
-domElement.search_result.insertAdjacentHTML("beforeend", list)
+domElement.search_result.insertAdjacentHTML("beforeend", list);
+// loading icon on the display until select food
+domElement.main.innerHTML = loading;
+
 }
 // to get value form user-input then pass it
 export const getInput = () => domElement.input__field.value;
